a5f741368ad9c76af062c69ac887938ca1fa85f1
Thanasis <thanasis.kap@gmail.com>
2020-02-01 21:45:17 +0200

    Blood trail

---
+++ Assets/Scripts/OrbitsSpace.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitsSpace : MonoBehaviour
+{
+    public Vector2 moveVector;
+    private Space orbitsSpace;
+    private ParticleSystem bloodTrailParticles;
+
+    private void Awake()
+    {
+        orbitsSpace = GetComponentInParent<Space>();
+        bloodTrailParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveVector * Time.deltaTime * 60f, UnityEngine.Space.Self);
+        if (transform.localPosition.x > orbitsSpace.bounds.x / 2)
+        {
+            PauseBloodTrail();
+            transform.localPosition = new Vector2(transform.localPosition.x - orbitsSpace.bounds.x, transform.localPosition.y);
+            ResumeBloodTrail();
+        }
+        else if (transform.localPosition.x < -orbitsSpace.bounds.x / 2)
+        {
+            PauseBloodTrail();
+            transform.localPosition = new Vector2(transform.localPosition.x + orbitsSpace.bounds.x, transform.localPosition.y);
+            ResumeBloodTrail();
+        }
+
+        if (transform.localPosition.y > orbitsSpace.bounds.y / 2)
+        {
+            PauseBloodTrail();
+            transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - orbitsSpace.bounds.y);
+            ResumeBloodTrail();
+        }
+        else if (transform.localPosition.x < -orbitsSpace.bounds.x / 2)
+        {
+            PauseBloodTrail();
+            transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + orbitsSpace.bounds.y);
+            ResumeBloodTrail();
+        }
+    }
+
+    private void ResumeBloodTrail()
+    {
+        ParticleSystem.EmissionModule emission = bloodTrailParticles.emission;
+        //emission.enabled = true;
+        bloodTrailParticles.Play();
+    }
+
+    private void PauseBloodTrail()
+    {
+        ParticleSystem.EmissionModule emission = bloodTrailParticles.emission;
+        bloodTrailParticles.Pause();
+        //emission.enabled = false;
+    }
+}
--- Assets/Scripts/StaticGameManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class StaticGameManager
-{
-    public static float GlobalTimeScale = 1f;
-}
